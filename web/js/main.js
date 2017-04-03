24be367c02eecd1ab5f52d27fb5e8621a3ab051c
Zakaria Elhariri <zakaria-elhariri@smile-maroc.com>
2017-04-03 17:13:48 +0100

    aze

--- web/js/main.js
+++ web/js/main.js
@@ -6,8 +6,3 @@ $(document).ready(function() {
   new Masonry('ol.annonces', {
     itemSelector: '.group'
   });
-
-  new Masonry('ol.advanced-filters-collections', {
-    itemSelector: '.singleAnnonce'
-  });
-})
